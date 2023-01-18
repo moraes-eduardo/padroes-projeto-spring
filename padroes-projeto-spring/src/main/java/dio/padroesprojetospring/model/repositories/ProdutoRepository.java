130db1b4ac5ecc5eff8bc3798a77db7194dfa2fb
moraes-eduardo <moraes-eduardo@hotmail.com>
2023-01-18 18:06:43 -0300

    Retirado anotação repository

--- padroes-projeto-spring/src/main/java/dio/padroesprojetospring/model/repositories/ProdutoRepository.java
+++ padroes-projeto-spring/src/main/java/dio/padroesprojetospring/model/repositories/ProdutoRepository.java
@@ -1,11 +1,9 @@
 package dio.padroesprojetospring.model.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import dio.padroesprojetospring.model.entities.Produto;
 
-@Repository
 public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
 
     public Iterable<Produto> findByDescricaoContainingIgnoreCase(String parteDescricao);
