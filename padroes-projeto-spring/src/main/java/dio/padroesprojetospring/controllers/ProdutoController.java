3e4458979ab5881b7c369f783ff7d63b7151691b
moraes-eduardo <moraes-eduardo@hotmail.com>
2023-01-18 15:22:02 -0300

    Ajustes no Controller

--- padroes-projeto-spring/src/main/java/dio/padroesprojetospring/controllers/ProdutoController.java
+++ padroes-projeto-spring/src/main/java/dio/padroesprojetospring/controllers/ProdutoController.java
@@ -5,9 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +21,8 @@ public class ProdutoController {
     @Autowired
 	public ProdutoRepository produtoRepository;
 	
-	@PostMapping
-	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Produto salvarProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
 	}
@@ -38,10 +37,5 @@ public class ProdutoController {
 		return produtoRepository.findById(id);
 	}
 	
-	@PutMapping
-	public Produto alterarProduto(@Valid Produto produto) {
-		produtoRepository.save(produto);
-		return produto;
-	}
-    
+ 
 }
